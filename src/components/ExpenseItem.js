e38efc74b1bb0beda4a12d923bbab8c70b0116b2
Piotr Domagalski <piotr.domagalski@yahoo.com>
2022-11-05 19:19:44 +0100

    first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div><h2>Title</h2></div>
+      <div>Ammount</div>
+    </div>
+  )
+}
