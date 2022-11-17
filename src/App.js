2ff4382193736f52aa50bea344e5537a336b09d3
Piotr Domagalski <piotr.domagalski@yahoo.com>
2022-11-17 19:03:35 +0100

    expenses filter fix

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
   ];
   const [expensesData, addExpense] = useState(expenses);
   const [filteredYear, changeYear] = useState('2021');
+  
 
   const addNewExpenseHandler = (newExpense) => {
     console.log(newExpense);
